33f902c3341ed8ea737869b54d5add3643e89fd7
Jon Gilbraith <jon@hobocode.io>
2015-05-02 11:58:53 +0100

    Ability to instantiate a client through their respecitve user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,13 @@ class User < ActiveRecord::Base
   has_one :twitter_login
 
   has_many :scheduled_updates
+
+  def twitter_client
+    @twitter_client ||= Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV['TWITTER_API_KEY']
+      config.consumer_secret     = ENV['TWITTER_API_SECRET']
+      config.access_token        = twitter_login.token
+      config.access_token_secret = twitter_login.secret
+    end
+  end
 end
