7580642673493e89c334bed90e9820dc9914fea2
Jon Gilbraith <jon@hobocode.io>
2015-05-02 12:30:33 +0100

    Simple method to tweet a scheduled update.

--- app/models/scheduled_update.rb
+++ app/models/scheduled_update.rb
@@ -5,4 +5,17 @@ class ScheduledUpdate < ActiveRecord::Base
   validates :user_id, presence: true
   validates :content, presence: true
   validates :due_at, presence: true
+
+  def tweet!
+    posted_tweet = user.twitter_client.update(content)
+
+    Tweet.create! do |tweet|
+      tweet.scheduled_update = self
+
+      tweet.twitter_id = posted_tweet.id
+      tweet.content = posted_tweet.text
+      tweet.url = posted_tweet.uri.to_s
+      tweet.timestamp = posted_tweet.created_at
+    end
+  end
 end
