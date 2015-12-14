552445f1ed812591a5359db10404170bed93b9f3
Jon Gilbraith <jon@hobocode.io>
2015-12-14 16:47:48 +0000

    Simple display of scheduled updates and posted tweets on home page.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,12 @@
 class PagesController < ApplicationController
 
   def home
-    render 'pages/home_logged_out' if !logged_in?
+    if logged_in?
+      @scheduled_updates = current_user.scheduled_updates.untweeted.order('due_at ASC')
+      @tweets = current_user.tweets.order('tweets.timestamp DESC')
+    else
+      render 'pages/home_logged_out'
+    end
   end
 
 end
