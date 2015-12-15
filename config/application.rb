3ada8b5dce3c2f552c3978ade4aa930c56895bdf
Jon Gilbraith <jon@hobocode.io>
2015-12-15 14:16:32 +0000

    Autoload lib directory.

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,8 @@ module Sochu
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Custom autoload paths
+    config.autoload_paths << Rails.root.join('lib')
   end
 end
