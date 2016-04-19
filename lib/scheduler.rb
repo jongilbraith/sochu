38ea3404bbb6bd2d14e9f365fd110b4952bbfa02
Jon Gilbraith <jon@hobocode.io>
2016-04-19 17:51:18 +0100

    Ensure all models are loaded so as to pull in any rules

--- lib/scheduler.rb
+++ lib/scheduler.rb
@@ -3,3 +3,10 @@ require_relative 'scheduler/adapter'
 
 module Scheduler
 end
+
+# Ensure all models are loaded so as to pull in any rules
+if !Rails.configuration.eager_load
+  Rails.root.join('app', 'models').each_child do |child|
+    require child if (child.file? && child.fnmatch?('*.rb'))
+  end
+end
